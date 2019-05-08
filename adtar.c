eecb42c42154be31e86fb6d9bbca3efd3bddf7bf
arantzardzm <arantzairdzm@gmail.com>
2019-05-08 16:30:29 +0400

    Checking if baseDirectory is in fact, a directory

--- adtar.c
+++ adtar.c
@@ -2,15 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int isDirectory(char *dir) {
+   struct stat fileInfo;
+   stat(dir, &fileInfo);
+   if (S_ISDIR(fileInfo.st_mode)){
+       return 1;
+   } else{
+       return 0;
+   }
+}
 
 void storeFiles(FILE *file, char *baseDirectory, int fileCount){
 
-    DIR *dir = opendir(baseDirectory);
+    DIR *dir;
+    struct dirent *dirp;
 
+    // Check if directory exists
     if ((dir = opendir(baseDirectory)) == NULL){
         printf("%s %s %s\n\n", "Error:", baseDirectory, "directory does not exist");
     } else {
         printf("%s %s %s\n\n", "Success: Opened", baseDirectory, "directory");
+
+        // Iterate through directory and its files
+        while ((dirp = readdir(dir)) != NULL){
+            char *base = baseDirectory;
+            char *name = dirp->d_name;
+
+            printf("%s, %s\n", base, name);
+            char *concat = strcat(base, name);
+
+            if (isDirectory(concat)) { // directory
+                printf("it is");
+                // storeFiles(archive, baseDir + dirp->d_name + "/", true, file_count);
+            } else {
+                printf("its is not");
+            }
+        }
     }
 
 }
